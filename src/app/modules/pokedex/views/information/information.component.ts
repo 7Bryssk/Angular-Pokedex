4ebfae0be1206e7f0e0dd21129e8ccce1965b488
Bruno Henrique <bruninho.carvalho159@gmail.com>
2018-11-06 11:04:01 -0200

    criando biblioteca para consumir o service

--- src/app/modules/pokedex/views/information/information.component.ts
+++ src/app/modules/pokedex/views/information/information.component.ts
@@ -1,7 +1,7 @@
 import { Component, OnInit, OnDestroy } from '@angular/core';
 import { ActivatedRoute } from '@angular/router';
 
-import { PokeapiService } from 'src/app/services/pokeapi.service';
+import { PokeapiService } from 'pokedata';
 import { takeWhile } from 'rxjs/operators';
 
 @Component({
